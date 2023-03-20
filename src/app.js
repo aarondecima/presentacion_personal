315e6fdefcc4fbd3a38b947e9c7f86cb7afeee47
aarondecima <aarondecima@gmail.com>
2023-03-20 20:21:50 -0300

    express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("hola mundo!!!")
+const express= require('express');
+const app= express();
+const PORT= 3001;
+
+app.get('/api',(req,res)=>{
+    console.log("paso x aca");
+    res.json({mensaje:'hola mundo'}).status(200);
+});
+app.listen(PORT,()=>{console.log('Estoy escuchando en el puerto: '+PORT)});
